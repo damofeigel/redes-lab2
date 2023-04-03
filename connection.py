f2fc742bc027cbbb8b4becf8f1efbf3e49f7314f
Damo2002 <damian.feigelmuller@mi.unc.edu.ar>
2023-04-03 02:05:55 -0300

    Cambio en el formato, [Errno 32] Broken pipe

--- connection.py
+++ connection.py
@@ -3,10 +3,14 @@
 # Copyright 2014 Carlos Bederián
 # $Id: connection.py 455 2011-05-01 00:32:09Z carlos $
 
-import socket
-import os 
+import os
 from constants import *
 from base64 import b64encode
+
+def create_error_msg(msg_code):
+    assert valid_status(msg_code)
+    buf = f"{msg_code} {error_messages[msg_code]}" + EOL
+    return buf
 
 class Connection(object):
     """
@@ -20,101 +24,127 @@ class Connection(object):
         self.socket = socket
         self.directory = directory
 
-    def create_error_msg(msg_code): 
-        buf = f"{msg_code}: {error_messages[msg_code]}" + EOL
-        return buf
-    
     def send(self, message, codification="ascii"):
+
         if codification == "b64encode":
             message = b64encode(message)
         if codification == "ascii":
             message = message.encode("ascii")
-    
+
         total_sent = 0
-        while message: 
+        while message:
             total_sent = self.socket.send(message)
             assert total_sent > 0
             message = message[total_sent:]
 
     def get_file_listing(self):
-        buf = Connection.create_error_msg(CODE_OK)
+        buf = create_error_msg(CODE_OK)
+
         for dir in os.listdir(self.directory):
             buf += dir + " " + EOL
-        buf + EOL
+        buf += EOL
 
         self.send(buf)
-        
+
     def get_metadata(self, filename):
         path = self.directory + '/' + filename
         # Check if file exists
         if not os.path.isfile(path):
-            self.send(error_messages[FILE_NOT_FOUND] + EOL)
+            self.send(create_error_msg(FILE_NOT_FOUND))
+            return
         # Check if filename is valid
+        valid = True
         for c in filename:
-            if (c == " "):
-                self.send(error_messages[INVALID_ARGUMENTS]+ EOL)
-        
-        buf = Connection.create_error_msg(CODE_OK);
-        filesize = os.stat(filename).st_size
-        buf += str(filesize) + EOL
+            if c not in VALID_CHARS:
+                valid = False
+                self.send(create_error_msg(INVALID_ARGUMENTS))
+                return
 
+        buf = create_error_msg(CODE_OK)
+        filesize = os.stat(path).st_size
+        buf += str(filesize) + EOL
         self.send(buf)
 
     def get_slice(self, filename, offset, size):
+        offset = int(offset)
+        size = int(size)
         path = self.directory + "/" + filename
         if not os.path.isfile(path):
-            self.send(Connection.create_error_msg(FILE_NOT_FOUND))
-        
-        filesize = os.stat(filename).st_size
+            self.send(create_error_msg(FILE_NOT_FOUND))
+            return
 
-        if offset and size < 0 :
-            self.send(Connection.create_error_msg(INVALID_ARGUMENTS))
+        filesize = os.stat(path).st_size
+
+        if offset < 0 and size < 0 :
+            self.send(create_error_msg(INVALID_ARGUMENTS))
+            return
+
         if offset > filesize:
-            self.send(error_messages(BAD_OFFSET))
+            self.send(create_error_msg(BAD_OFFSET))
+            return
 
-        buf = error_messages()
+        if offset + size > filesize:
+            self.send(create_error_msg(BAD_OFFSET))
+            return
+
         with open(path, 'r') as file:
             file.seek(offset)
             buf = file.read(size)
         # Codificamos a base64 y enviamos
-        buf64_bytes = b64encode(buf.b64encode('ascii'))
-        buf64 = b64encode.decode('ascii')
-    
-        buf = error_messages[CODE_OK] + EOL + buf64 + EOL 
-        self.send(buf, )
-  
+        buf64_bytes = b64encode(buf.encode('ascii'))
+        buf64 = buf64_bytes.decode('ascii')
+
+        buf = create_error_msg(CODE_OK) + buf64 + EOL
+        self.send(buf)
+
     def quit(self):
-        self.send(Connection.create_error_msg(CODE_OK) + EOL)
-        self.socket.close()
-        
+        self.send(create_error_msg(CODE_OK))
+        #self.socket.close()
+
     def handle(self):
         """
         Atiende eventos de la conexión hasta que termina.
         """
         while True:
-            # el mensaje deberia ser un comando   
-            data = self.socket.recv(4096).decode('ascii') 
+            # el mensaje deberia ser un comando
+            data = self.socket.recv(4096).decode('ascii')
             if len(data) == 0:
-                self.send(error_messages(BAD_REQUEST))
+                self.send(create_error_msg(BAD_REQUEST))
                 break
-            
+
+            if '/n' in data:
+                self.send(create_error_msg(BAD_EOL))
+                break
+
             argv = data.split()
-            match argv[0]:
-                case "get_file_listing":
-                    # Dar error si hay otro argumento?
-                    self.get_file_listing()
-                case "get_metadata":
-                    # la misma funcion checkea que el 
-                    # segundo argumento sea valido
-                    if len(argv) != 2:
-                        self.send(error_messages(BAD_REQUEST))
-                    self.get_metadata(argv[1])
-                case "get_slice":
-                    if len(argv) != 4:
-                        self.send(Connection.create_error_msg(INVALID_ARGUMENTS))
-                    self.get_slice(argv[1], argv[2], argv[3])
-                case "quit":
-                    self.quit()
-                    break 
-                case _:
-                    self.send(Connection.create_error_msg(INVALID_COMMAND))
+
+            try:
+                match argv:
+                    case ['get_file_listing']:
+                        self.get_file_listing()
+
+                    case ['get_metadata', filename]:
+                        self.get_metadata(filename)
+
+                    case ['get_slice', filename, offset, size]:
+                        if not (offset.isdigit() and size.isdigit()):
+                            self.send(create_error_msg(INVALID_ARGUMENTS))
+                        else:
+                            self.get_slice(filename, offset, size)
+
+                    case ['quit']:
+                        self.quit()
+                        break
+
+                    case (
+                            ['get_file_listing', *_] | ['get_metadata', *_] |
+                            ['get_file_slice', *_] | ['quit', *_] ):
+                        self.send(create_error_msg(INVALID_ARGUMENTS))
+
+                    case _:
+                        self.send(create_error_msg(INVALID_COMMAND))
+            except Exception:
+                self.send(create_error_msg(INTERNAL_ERROR))
+                break
+
+        self.socket.close()
